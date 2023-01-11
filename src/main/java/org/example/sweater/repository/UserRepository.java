8e1284d632181288680eaf69fb538c95f341fd6a
Matthew Zhigalo <matthewzhigalo@gmail.com>
2023-01-12 01:20:52 +0300

    .gitignore changes

--- src/main/java/org/example/sweater/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.sweater.repository;
-
-import org.example.sweater.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    User findByUsername(String username);
-}
