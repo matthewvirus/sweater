949ea2c4c2f33d94270ac11cdc39966a564bcf18
Matthew Zhigalo <matthewzhigalo@gmail.com>
2023-01-19 18:25:04 +0300

    Added new service methods

--- src/main/java/by/matthewvirus/sweater/service/UserService.java
+++ src/main/java/by/matthewvirus/sweater/service/UserService.java
@@ -1,10 +1,15 @@
 package by.matthewvirus.sweater.service;
 
+import by.matthewvirus.sweater.entity.Role;
+import by.matthewvirus.sweater.entity.User;
 import by.matthewvirus.sweater.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -17,6 +22,52 @@ public class UserService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByUsername(username);
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found!");
+        }
+        return user;
+    }
+
+    public User findUserById(Long id) {
+        Optional<User> userFromDb = userRepository.findById(id);
+        return userFromDb.orElse(new User());
+    }
+
+    public List<User> allUsers() {
+        return userRepository.findAll();
+    }
+
+    public boolean saveUser(User user) {
+        User userFromDb = userRepository.findByUsername(user.getUsername());
+        if (userFromDb != null) {
+            return false;
+        }
+        user.setActive(true);
+        user.setRoleSet(Collections.singleton(Role.ROLE_USER));
+        userRepository.save(user);
+        return true;
+    }
+
+    public boolean deleteUser(Long id) {
+        if (userRepository.findById(id).isPresent()) {
+            userRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    public void updateUser(String username, Map<String, String> form, User user) {
+        user.setUsername(username);
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+        user.getRoleSet().clear();
+        for (String key: form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoleSet().add(Role.valueOf(key));
+            }
+        }
+        userRepository.save(user);
     }
 }
