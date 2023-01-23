1608da56ba1d7aa5a33ff406153fe1c4b185cd70
Matthew Zhigalo <matthewzhigalo@gmail.com>
2023-01-23 19:07:17 +0300

    Refactored error system

--- src/main/java/by/matthewvirus/sweater/controller/RegistrationController.java
+++ src/main/java/by/matthewvirus/sweater/controller/RegistrationController.java
@@ -2,9 +2,12 @@ package by.matthewvirus.sweater.controller;
 
 import by.matthewvirus.sweater.entity.User;
 import by.matthewvirus.sweater.service.UserService;
+import by.matthewvirus.sweater.util.ControllerUtils;
+import jakarta.validation.Valid;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,9 +27,24 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String addUser(@NotNull User user, Model model) {
+    public String addUser(
+            @NotNull @Valid User user,
+            BindingResult bindingResult,
+            Model model)
+    {
+        if (user.getPasswordConf() == null || user.getPasswordConf().isEmpty()) {
+            model.addAttribute("passwordConfError", "Password confirmation can't be empty!");
+        }
+        if (user.getPassword() != null && !user.getPassword().equals(user.getPasswordConf())) {
+            model.addAttribute("passwordsError", "Passwords are not equals!");
+            System.out.println(user.getPasswordConf() + user.getPassword());
+        }
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("errorsMap", ControllerUtils.getErrors(bindingResult));
+            return "registration";
+        }
         if (!userService.saveUser(user)) {
-            model.addAttribute("message", "User is exists");
+            model.addAttribute("usernameError", "User is exists");
             return "registration";
         }
         return "redirect:/login";
